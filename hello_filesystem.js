4f7fff74c7ca6f89df82a05ac50cffe6661dec6d
collins peng <collins.peng@outlook.com>
2020-02-05 23:41:42 +0800

    adding profile and starter

--- hello_filesystem.js
+++ hello_filesystem.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 
 http.createServer(function(req, res) {
@@ -20,6 +21,6 @@ http.createServer(function(req, res) {
 		console.log("... incoming request: " + req.url);
 		return res.end();
 });
-}).listen(8080); 
+}).listen(PORT); 
 
 console.log('Server listening port 8080...')
